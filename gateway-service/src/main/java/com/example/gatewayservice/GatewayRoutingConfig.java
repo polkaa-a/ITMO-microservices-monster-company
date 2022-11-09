de9feac7b6fff0a0ade7ed18b4c9bf9a1fd45eb2
Polina Popova <cherrywood6918@gmail.com>
2022-11-09 15:17:03 +0300

    Initial commit

---
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayRoutingConfig.java
@@ -0,0 +1,20 @@
+package com.example.gatewayservice;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRoutingConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", route -> route.path("/users/**")
+                        .uri("http://localhost:8081"))
+                .route("user-service", route -> route.path("/roles/**")
+                        .uri("http://localhost:8081"))
+                .build();
+    }
+}
---
+++ user-service/src/main/java/com/example/userservice/UserController.java
@@ -0,0 +1,17 @@
+package com.example.userservice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "hello";
+    }
+}
