2a27b02bd0843c64e6a88d9a0ca69a9ce8341e69
Ekaterina <ekaterina.2001@live.com>
2022-11-14 02:20:15 +0300

    initial commit

---
+++ monster-service/src/main/java/monsters/MonsterServiceApplication.java
@@ -0,0 +1,12 @@
+package monsters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonsterServiceApplication.class, args);
+    }
+
+}
