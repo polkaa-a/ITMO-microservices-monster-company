5334a5345c7be4a93d9376c06eea8bd40cb9b23f
Ekaterina <ekaterina.2001@live.com>
2022-11-21 14:55:20 +0300

    feat: add InfectionDTO (infection-service) for Feign Client

---
+++ monster-service/src/main/java/monsters/dto/InfectionDTO.java
@@ -0,0 +1,28 @@
+package monsters.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import java.sql.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+public class InfectionDTO {
+
+    private UUID id;
+
+    @NotNull(message = "shouldn't be null")
+    private UUID monsterId;
+
+    @NotNull(message = "shouldn't be null")
+    private UUID infectedThingId;
+
+    @NotNull(message = "shouldn't be null")
+    private Date infectionDate;
+
+    private Date cureDate;
+}
