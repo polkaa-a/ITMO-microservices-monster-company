079485878b4c4bc53082ca9e20b281e65372fe04
Polina Popova <cherrywood6918@gmail.com>
2022-11-21 14:38:25 +0300

    feat(gateway-service): routing

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayRoutingConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayRoutingConfig.java
@@ -1,20 +1,36 @@
 package com.example.gatewayservice;
 
+import com.example.gatewayservice.authorization.RoleAuthGatewayFilterFactory;
+import com.example.gatewayservice.authorization.RoleAuthGatewayFilterFactory.Config;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class GatewayRoutingConfig {
+import java.util.List;
 
+@Configuration
+@RequiredArgsConstructor
+public class GatewayRoutingConfig {
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder,
+                                           RoleAuthGatewayFilterFactory authFactory) {
         return builder.routes()
                 .route("user-service", route -> route.path("/users/**")
+                       .filters(f -> f.filter(authFactory.apply(
+                               new Config(List.of("ADMIN","SCARER"))
+                        )))
                         .uri("http://localhost:8081"))
                 .route("user-service", route -> route.path("/roles/**")
+                        .filters(f -> f.filter(authFactory.apply(
+                                new Config(List.of("ADMIN"))
+                        )))
+                        .uri("http://localhost:8081"))
+                .route("user-service", route -> route.path("/auth")
                         .uri("http://localhost:8081"))
                 .build();
     }
 }
+
+
