b07bd9f3267d84bda6e5303c138a187cc28bc96a
Ekaterina <ekaterina.2001@live.com>
2022-11-24 16:23:51 +0300

    fix: reactive feign client and datasource ports

--- monster-service/src/main/java/monsters/service/feign/clients/InfectionServiceFeignClient.java
+++ monster-service/src/main/java/monsters/service/feign/clients/InfectionServiceFeignClient.java
@@ -1,16 +1,16 @@
 package monsters.service.feign.clients;
 
 import monsters.dto.InfectionDTO;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import reactivefeign.spring.config.ReactiveFeignClient;
 
 import java.util.Date;
 import java.util.List;
 
-@ReactiveFeignClient(name = "infection-service", url = "http://localhost:8083/")
+@FeignClient(name = "infection-service", url = "http://localhost:8083/")
 public interface InfectionServiceFeignClient {
     @GetMapping("/infections/{date}")
     @ResponseStatus(HttpStatus.OK)
