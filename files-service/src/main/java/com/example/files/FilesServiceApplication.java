add3018e744c9244ee3c9e399fa7aa2dec98fc17
polkaa_a <ekaterina.2001@live.com>
2022-12-21 01:05:41 +0300

    feat: add file-service

---
+++ files-service/src/main/java/com/example/files/FilesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.filesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesServiceApplication.class, args);
+    }
+
+}
---
+++ files-service/src/main/java/com/example/files/controller/exception/object/GetObjectException.java
@@ -0,0 +1,7 @@
+package com.example.files.controller.exception.object;
+
+public class GetObjectException extends RuntimeException {
+    public GetObjectException(String message) {
+        super(message);
+    }
+}
